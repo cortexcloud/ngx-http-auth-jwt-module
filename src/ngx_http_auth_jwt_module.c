68151a7b35bfa6dfff05b6af52712c6ea83e506e
Ming <pawat@hlabconsulting.com>
2022-11-28 22:58:51 +0700

    If no snowstorm roles redirect to 401

--- src/ngx_http_auth_jwt_module.c
+++ src/ngx_http_auth_jwt_module.c
@@ -299,6 +299,7 @@ static ngx_int_t ngx_http_auth_jwt_handler(ngx_http_request_t *r)
         if (roles == NULL)
         {
             ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "the JWT does not contain an roles");
+            goto redirect;
         }
         else
         {
